4a4b9bc0b4465de8909f8693d41a09b5f56124fc
Keiran <keirankozlowski@gmail.com>
2021-01-14 20:10:39 -0500

    initial push before restarting vscode

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { render } from 'react-dom'
 // 👉 App contains a more sophisticated form we'll flesh out later
 import App from './components/App'
@@ -10,8 +10,40 @@ const petsList = [
   { petName: 'Goldie', petType: 'fish' },
 ]
 
+const initialFormValues = { petName: '', petType: '' }
+
 function SimpleForm() {
-  return <div>Ready to start GP!</div>
+  const [pets, setPets] = useState(petsList);
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const change = (e) => {
+    console.log(e);
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  }
+
+  return (
+    <div>
+      {
+        pets.map((pet, i) => (
+          <div>
+            {pet.petName} is a {pet.petType}
+          </div>
+        ))
+      }
+
+      <form>
+        <input
+          name="petName"
+          value="hello"
+          onChange={change} />
+        <input
+          name="petType"
+          value="world"
+          onChange={change} />
+      </form>
+    </div>
+  );
 }
 
 render(
